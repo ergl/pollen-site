9ca91644d7efe30389d885262f89cfea21707afe
ergl <ergl@users.noreply.github.com>
2019-07-24 13:31:07 +0200

    Use ↩ for footnote backlinks

--- util-footnotes.rkt
+++ util-footnotes.rkt
@@ -15,9 +15,22 @@
   (define target-href (format "#fn-~a" note-number))
   (define src-id (format "fn-source-~a" note-number))
   (define src-href (format "#fn-source-~a" note-number))
+
+  ; Append to the footnote list
+  ; We'll get back to it when we build the entire document
   (set! footnote-list
-        (append footnote-list (list `(p ([id ,target-id]) ,(format "~a. " note-number) ,@contents (sup (a ([href ,src-href] [class "footnote-backlink"]) "[return]︎"))))))
+        (append footnote-list
+                (list `(p ([id ,target-id])
+                          ,(format "~a. " note-number)
+                          ,@contents
+                          (a ([href ,src-href] [class "footnote-backlink"]) "↩")))))
+
   `(sup (a ([href ,target-href] [id ,src-id] [class "footnote-forwardlink"]) ,(format "~a" note-number))))
 
 (define (add-to-doc tx)
-  (txexpr (get-tag tx) (get-attrs tx) `(,@(get-elements tx) ,(when/splice (not (empty? footnote-list)) `(hr) `(h3 "Notes")) ,@footnote-list)))
+  (txexpr (get-tag tx)
+          (get-attrs tx)
+          `(,@(get-elements tx)
+            ,(when/splice (not (empty? footnote-list))
+            `(hr)
+            `(h3 "Notes")) ,@footnote-list)))
