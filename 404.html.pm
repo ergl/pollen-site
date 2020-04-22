2968b2bea83c7e79b8581a2d0ddda96ef7c37fb9
ergl <ergl@users.noreply.github.com>
2020-04-22 16:22:31 +0200

    Add 404 page

---
+++ 404.html.pm
@@ -0,0 +1,10 @@
+#lang pollen
+◊(define-meta disable-nav null)
+
+◊h1{Uh oh!}
+
+Looks like you've found a broken link somewhere. Head over to ◊l["/"]{the main page} and try again?
+
+◊hr{}
+
+If you think this page should ◊em{definitely} exist, feel free to ◊l["mailto:borjaocook@gmail.com"]{email me}.
