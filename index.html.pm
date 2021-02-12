a573ffe52bc3ade18de78ba04acd9a4e63c9ae0b
ergl <ergl@users.noreply.github.com>
2021-02-12 21:16:05 +0100

    Update home page

--- index.html.pm
+++ index.html.pm
@@ -4,14 +4,13 @@
 
 I'm Borja de Régil. I'm currently a research engineer at the
 ◊l["https://software.imdea.org" #:ext #t]{IMDEA Software Institute} working on
-cool dist-sys things. If you wish to know more, or want to check
-out things I've worked on, just head over to the ◊l["/about.html"]{about} section.
+distributed systems with ◊l["http://software.imdea.org/~gotsman/" #:ext #t]{Alexey Gotsman}. Head over to the ◊l["/about.html"]{about} section if you wish to know more, or want to check out things I've worked on.
 
 You can find some of my opinions on the rarely-updated ◊l["/posts"]{blog}. Opinions are mostly wrong ◊em{all} the time, but I hope you can find some of them useful. I also keep (or try to) a ◊l["/wiki"]{personal wiki}, where I keep looser notes on books and articles I read, and even more opinions I might or might not have anymore.
 
 ◊; TODO(borja): insert link to "social media" category here?
 I don't do much social media, but I do have a ◊l["https://github.com/ergl" #:ext #t]{Github account}.
-I tried to use Twitter two times in my life and both times I ended up feeling miserable.
+I tried to use Twitter two times in my life and both times I realized it was not for me.
 However, feel free to reach out by ◊l["mailto:borjaocook@gmail.com"]{email} or
 ◊l["http://t.me/king_of_manlets" #:ext #t]{Telegram}, I'm always open to ideas,
 comments, or just general electronic banter.
