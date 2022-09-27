06941273e8421208fc9acb3084c4540975b6fdc7
ergl <ergl@users.noreply.github.com>
2022-09-27 17:04:41 +0200

    Reword main page

--- index.html.pm
+++ index.html.pm
@@ -6,13 +6,13 @@ I'm Borja de Régil. I'm currently a research engineer at the
 ◊l["https://software.imdea.org" #:ext #t]{IMDEA Software Institute} working on
 distributed systems with ◊l["http://software.imdea.org/~gotsman/" #:ext #t]{Alexey Gotsman}. Head over to the ◊l["/about.html"]{about} section if you wish to know more, or want to check out things I've worked on.
 
-You can find some of my opinions on the rarely-updated ◊l["/posts"]{blog}. Opinions are mostly wrong ◊em{all} the time, but I hope you can find some of them useful. I also keep (or try to) a ◊l["/wiki"]{personal wiki}, where I keep looser notes on books and articles I read, and even more opinions I might or might not have anymore.
+You can find some of my thoughts on the rarely-updated ◊l["/posts"]{blog}. I might
+have changed my mind about some of the things written there, but I hope you can find something useful. I also keep (or try to) a ◊l["/wiki"]{personal wiki}, where I keep looser notes on books and articles I read.
 
 ◊; TODO(borja): insert link to "social media" category here?
-I don't do much social media, but I do have a ◊l["https://github.com/ergl" #:ext #t]{Github account}.
-I tried to use Twitter two times in my life and both times I realized it was not for me.
-However, feel free to reach out by ◊l["mailto:borjaocook@gmail.com"]{email} or
-◊l["http://t.me/king_of_manlets" #:ext #t]{Telegram}, I'm always open to ideas,
+Feel free to reach out by ◊l["mailto:borjaocook@gmail.com"]{email} or
+◊l["http://t.me/king_of_manlets" #:ext #t]{Telegram}, or check out
+my ◊l["https://github.com/ergl" #:ext #t]{Github account}. I'm always open to ideas,
 comments, or just general electronic banter.
 
 Feel free to ◊l[◊|atom-path|]{subscribe via RSS}.
