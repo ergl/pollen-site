aa05b9309cded264be18b3c0dbb0ab20503c71aa
ergl <ergl@users.noreply.github.com>
2022-10-09 19:17:21 +0200

    Change job title

--- index.html.pm
+++ index.html.pm
@@ -2,9 +2,8 @@
 
 ◊h2{Hi!}
 
-I'm Borja de Régil. I'm currently a research engineer at the
-◊l["https://software.imdea.org" #:ext #t]{IMDEA Software Institute} working on
-distributed systems with ◊l["http://software.imdea.org/~gotsman/" #:ext #t]{Alexey Gotsman}. Head over to the ◊l["/about.html"]{about} section if you wish to know more, or want to check out things I've worked on.
+I'm Borja de Régil. I'm currently a software engineer at ◊l["https://www.ditto.live/" #:ext #t]{Ditto} working on a distributed database powered by CRDTs. Before that, I
+was a distributed systems research engineer at the ◊l["https://software.imdea.org" #:ext #t]{IMDEA Software Institute}. Head over to the ◊l["/about.html"]{about} section if you wish to know more, or want to check out things I've worked on.
 
 You can find some of my thoughts on the rarely-updated ◊l["/posts"]{blog}. I might
 have changed my mind about some of the things written there, but I hope you can find something useful. I also keep (or try to) a ◊l["/wiki"]{personal wiki}, where I keep looser notes on books and articles I read.
